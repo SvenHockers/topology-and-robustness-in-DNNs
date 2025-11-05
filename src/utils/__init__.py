3120731ec11c9f10a1e9b27c4f4a149f14ed3f83
SvenHockers <90685770+SvenHockers@users.noreply.github.com>
2025-11-05 15:24:44 +0100

    point cloud generation and some basic transformations

---
+++ src/utils/__init__.py
@@ -0,0 +1,25 @@
+from .point_cloud import (
+    TransformSpec,
+    add_gaussian_noise,
+    apply_transforms,
+    generate_point_cloud,
+    random_rotation_matrix,
+    rotate_points,
+    scale_points,
+    shape_to_point_cloud,
+    translate_points,
+)
+
+__all__ = [
+    "TransformSpec",
+    "add_gaussian_noise",
+    "apply_transforms",
+    "generate_point_cloud",
+    "random_rotation_matrix",
+    "rotate_points",
+    "scale_points",
+    "shape_to_point_cloud",
+    "translate_points",
+]
+
+
